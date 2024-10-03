dc738a5041b57e9406190b634917a063895c9bc1
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2024-10-03 20:42:06 +0200

    embeddings test + notes

--- draft/llamaIndex.ts
+++ draft/llamaIndex.ts
@@ -1,1 +1,16 @@
-console.log("alo")
+// Exec with the following command `npx tsx draft/llamaIndex.ts`
+import { 
+  HuggingFaceEmbedding, 
+} from "llamaindex";
+
+// Model metadata and onnx file is store at node_modules/@xenova/transformers/.cache
+const embedModel = new HuggingFaceEmbedding({
+  modelType: "Alibaba-NLP/gte-large-en-v1.5",
+  quantized: false,
+});
+
+(async() => {
+  const embeddings = await embedModel.getTextEmbedding("Tung dep trai")
+  console.log(embeddings.length)
+  console.log(embeddings.slice(0,5))
+})();
---
+++ notes.md
@@ -0,0 +1,5 @@
+# My Notes 
+
+List of supported vector DB by LlamaIndex : https://docs.llamaindex.ai/en/stable/module_guides/storing/vector_stores/
+
+The API in TypeScript and Python of LlamaIndex are pretty similar. We can read Python docs in case TS doc is dogshit.
