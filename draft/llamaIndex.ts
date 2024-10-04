54f05082d42724839f93fd68893d75c8c1ac6615
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2024-10-04 12:27:10 +0200

    try weaviate embedded

--- draft/llamaIndex.ts
+++ draft/llamaIndex.ts
@@ -1,7 +1,12 @@
 // Exec with the following command `npx tsx draft/llamaIndex.ts`
 import { 
-  HuggingFaceEmbedding, 
+  HuggingFaceEmbedding,
+  SimpleDirectoryReader,
+  SimpleNodeParser,
+  WeaviateVectorStore, 
 } from "llamaindex";
+import weaviateV2, { EmbeddedOptions} from 'weaviate-ts-embedded';
+import weaviateV3 from 'weaviate-client'
 
 // Model metadata and onnx file is store at node_modules/@xenova/transformers/.cache
 const embedModel = new HuggingFaceEmbedding({
@@ -11,6 +16,46 @@ const embedModel = new HuggingFaceEmbedding({
 
 (async() => {
   const embeddings = await embedModel.getTextEmbedding("Tung dep trai")
-  console.log(embeddings.length)
+  const dim = embeddings.length
   console.log(embeddings.slice(0,5))
+  console.log(dim)
+
+  // Load dir 
+  const dirReader = new SimpleDirectoryReader()
+  const blogs = await dirReader.loadData('./draft/data/')
+  const parser = new SimpleNodeParser({
+    chunkSize : 1024,
+    chunkOverlap : 20
+  })
+  const nodes = parser.getNodesFromDocuments(blogs)
+
+  // Start embedded server 
+  const clientV2 = weaviateV2.client(
+    new EmbeddedOptions({
+      env: {
+        ENABLE_MODULES: "text2vec-huggingface",
+      },
+    })
+  );
+  await clientV2.embedded.start();
+
+  const liveCheck = await clientV2.misc
+  .liveChecker()
+  .do();
+
+  console.log("Send heartbeats Weaviate : " + liveCheck)
+
+  // Map to v3 because WeaviateVectorStore only accept v3 client
+  let clientV3
+  // Create v3 server 
+  try {
+    clientV3 = await weaviateV3.connectToLocal({
+      port : 6666,
+    })
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+
+  console.log(clientV3)
+
 })();
--- notes.md
+++ notes.md
@@ -3,3 +3,11 @@
 List of supported vector DB by LlamaIndex : https://docs.llamaindex.ai/en/stable/module_guides/storing/vector_stores/
 
 The API in TypeScript and Python of LlamaIndex are pretty similar. We can read Python docs in case TS doc is dogshit.
+
+`stella en 400M v5` model's ONNX file needs to be downloaded from [this commit](https://huggingface.co/dunzhang/stella_en_400M_v5/tree/refs%2Fpr%2F3)
+
+Milvus lite doesn't work with Typescript
+
+No hugging face integration for TS SDK v2 [link](https://weaviate.io/developers/weaviate/model-providers/transformers/embeddings#weaviate-configuration)
+
+LlamaIndex only works with weaviate v3 client => embedded weaviate is not possible
